13e7a822690f68d1c0381dd6b74585e51a1b9d44
Gohans <khoaphamda@gmail.com>
2025-12-05 22:32:42 +0700

    1

--- main.ts
+++ main.ts
@@ -1,12 +1,6 @@
 import { App, debounce, Plugin, PluginSettingTab, Setting } from "obsidian";
-import EMERGE_MOTION from "./animations/gemmy_emerge.gif";
-import POP_MOTION from "./animations/gemmy_pop.gif";
-import DISAPPEAR_MOTION from "./animations/gemmy_disappear.gif";
-import ANGRY_MOTION from "./animations/gemmy_angry.gif";
-import LOOK_MOTION from "./animations/gemmy_lookAround.gif";
-import IDLE_MOTION from "./animations/gemmy_idle.gif";
-import DISAPPOINT_IMG from "./animations/gemmy_disappoint.gif";
 import * as data from "./quotes_corrected.json";
+import KAPILGUPTA_STATIC from "./kapilgupta.png";
 
 const ALL_QUOTES = data.quotes;
 
@@ -91,7 +85,7 @@ export default class Gemmy extends Plugin {
 				return;
 			}
 
-			this.imageEl.setAttribute("src", IDLE_MOTION);
+			this.imageEl.setAttribute("src", KAPILGUPTA_STATIC);
 			this.startNextIdleTimeout();
 		});
 
@@ -118,25 +112,8 @@ export default class Gemmy extends Plugin {
 	appear() {
 		let { gemmyEl, imageEl } = this;
 
-		imageEl.setAttribute("src", EMERGE_MOTION);
-
-		// Quicker if we're in writing mode
-		if (this.inWritingMode) {
-			imageEl.setAttribute("src", POP_MOTION);
-
-			setTimeout(() => {
-				this.appeared = true;
-
-				this.saySomething(ALL_QUOTES, true);
-			}, 1800);
-		} else {
-			imageEl.setAttribute("src", EMERGE_MOTION);
-
-			setTimeout(() => {
-				imageEl.setAttribute("src", IDLE_MOTION);
-				this.appeared = true;
-			}, 3800);
-		}
+		imageEl.setAttribute("src", KAPILGUPTA_STATIC); // Thay hết bằng ảnh tĩnh
+		this.appeared = true; // Cho nó hiện ra luôn, chờ cái lồn gì nữa
 
 		document.body.appendChild(gemmyEl);
 		gemmyEl.show();
@@ -146,7 +123,8 @@ export default class Gemmy extends Plugin {
 		this.idleTimeout && window.clearTimeout(this.idleTimeout);
 		this.writingModeTimeout && window.clearTimeout(this.writingModeTimeout);
 
-		this.imageEl.setAttribute("src", DISAPPEAR_MOTION);
+		// Vứt mẹ animation đi, ảnh nào thì cũng là ảnh tĩnh thôi
+		this.imageEl.setAttribute("src", KAPILGUPTA_STATIC);
 		// remote tooltip
 		this.gemmyEl.dispatchEvent(
 			new MouseEvent("mouseout", {
@@ -155,10 +133,10 @@ export default class Gemmy extends Plugin {
 				clientY: 10,
 			}),
 		);
-		setTimeout(() => {
-			this.gemmyEl.hide();
-			this.appeared = false;
-		}, 1300);
+
+		// Ẩn luôn và ngay, chờ cái lồn gì nữa
+		this.gemmyEl.hide();
+		this.appeared = false;
 	}
 
 	enterWritingMode() {
@@ -224,16 +202,11 @@ export default class Gemmy extends Plugin {
 			}),
 		);
 
-		if (this.inWritingMode) {
-			this.imageEl.setAttribute("src", ANGRY_MOTION);
-			setTimeout(() => {
-				this.imageEl.setAttribute("src", DISAPPOINT_IMG);
-			}, 1000);
-		} else {
-			this.imageEl.setAttribute("src", LOOK_MOTION);
-		}
+		// Ảnh lúc lồn nào cũng là 1, đéo cần if else xàm cặc
+		this.imageEl.setAttribute("src", KAPILGUPTA_STATIC);
 
 		if (!persistent) {
+			// Chỉ lo ẩn cái bubble thôi, ảnh đéo cần đổi
 			setTimeout(() => {
 				this.gemmyEl.dispatchEvent(
 					new MouseEvent("mouseout", {
@@ -242,7 +215,6 @@ export default class Gemmy extends Plugin {
 						clientY: 10,
 					}),
 				);
-				this.imageEl.setAttribute("src", IDLE_MOTION);
 			}, BUBBLE_DURATION);
 		}
 	}
